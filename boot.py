ae98ef65a60876d203727f4ba8970b3dd7b59d10
JensDiemer <git@jensdiemer.de>
2019-11-10 16:30:03 +0100

    Accept more than one Wifi SSID/Passwords in config.json
    and try to scan/connect more than one time

--- boot.py
+++ boot.py
@@ -6,6 +6,7 @@
 import gc
 gc.collect()
 
+import time
 import machine
 led_pin = machine.Pin(13, machine.Pin.OUT, value=0) # turn power LED on
 
@@ -20,17 +21,52 @@ if ap.active():
     ap.active(False)
 
 wlan = network.WLAN(network.STA_IF) # create station interface
+wlan.active(False)
 wlan.active(True)       # activate the interface
-for info in wlan.scan():
-    ssid, bssid, channel, RSSI, authmode, hidden = info
-    ssid = ssid.decode("UTF-8")
-    print('SSID:', ssid, '(channel:', channel, 'hidden:', hidden, ')')
+
+def get_known_ssid(wlan, config):
+    print('Scan WiFi...')
+    known_ssid = None
+    for no in range(3):
+        for info in wlan.scan():
+            ssid, bssid, channel, RSSI, authmode, hidden = info
+            ssid = ssid.decode("UTF-8")
+            print('SSID:', ssid, '(channel:', channel, 'hidden:', hidden, ')')
+            if ssid in wifi_configs:
+                known_ssid = ssid
+        if known_ssid is not None:
+            return known_ssid
+    print('ERROR: No known WiFi found!')
+
+
+def connect(wlan, ssid, password):
+    for no in range(3):
+        print('Connect to Wifi access point:', ssid, repr(password))
+        wlan.connect(ssid, password)
+        time.sleep(1)
+        if wlan.isconnected():
+            print('Connected to:', ssid)
+            return
+        else:
+            print('Try again...')
+            led_pin.value(1) # turn power LED off
+            wlan.active(False)
+            time.sleep(2)
+            led_pin.value(0) # turn power LED on
+            wlan.active(True)
+    print("ERROR: WiFi not connected! Password wrong?!?")
+
 
 if not wlan.isconnected():
     from get_config import config
-    ssid = config['wifi']['ssid']
-    print('Connect to Wifi access point:', ssid)    
-    wlan.connect(ssid, config['wifi']['password'])
+    wifi_configs = config['wifi']
+    
+    ssid = get_known_ssid(wlan, config)
+    if ssid is None:
+        print('Skip Wifi connection.')
+    else:
+        connect(wlan, ssid, password=config['wifi'][ssid])
+            
 else:
     print('already connected!')
     
