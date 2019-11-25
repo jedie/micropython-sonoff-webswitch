4599866492aa8028bc7db7edaf40437ad5d715d7
JensDiemer <git@jensdiemer.de>
2019-11-25 17:52:48 +0100

    Reset the device by long pressing the button.

--- src/main.py
+++ src/main.py
@@ -14,43 +14,13 @@ from watchdog import watchdog  # noqa isort:skip
 from wifi import wifi  # noqa isort:skip
 from ntp import ntp_sync  # noqa isort:skip
 from leds import power_led  # noqa isort:skip
+import button_handler  # noqa isort:skip
 
 print('watchdog:', watchdog)
 print('wifi:', wifi)
 print('ntp_sync:', ntp_sync)
 print('power_led:', power_led)
 
-
-def get_debounced_value(pin):
-    """get debounced value from pin by waiting for 20 msec for stable value"""
-    cur_value = pin.value()
-    stable = 0
-    while stable < 20:
-        if pin.value() == cur_value:
-            stable = stable + 1
-        else:
-            stable = 0
-            cur_value = pin.value()
-    time.sleep_ms(1)
-    return cur_value
-
-
-def button_pressed(pin):
-    print('button pressed...')
-    cur_button_value = get_debounced_value(pin)
-    if cur_button_value == 1:
-        if relay_pin.value() == 1:
-            print('turn off by button.')
-            relay_pin.value(0)
-        else:
-            print('turn on by button.')
-            relay_pin.value(1)
-
-        garbage_collection()
-
-
-button_pin = machine.Pin(0, machine.Pin.IN)
-button_pin.irq(button_pressed)
 
 
 print('gc.collect()')
