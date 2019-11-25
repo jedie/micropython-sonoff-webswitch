eb34d2f786925cfdd681c1babb5c3669ca7206bf
JensDiemer <git@jensdiemer.de>
2019-11-25 17:48:32 +0100

    Don't go into sleep mode

--- src/boot.py
+++ src/boot.py
@@ -3,19 +3,19 @@ import gc
 import esp
 import utime as time
 
+print('gc.collect()')
+gc.collect()
 
-for no in range(3, 0, -1):
+
+for no in range(2, 0, -1):
     print('%i boot.py wait...' % no)
     time.sleep(1)
 
 
 print('Check Firmware:')
 assert esp.check_fw() is True, "Firmware error?!?"
-esp.osdebug(None)       # turn off vendor O/S debugging messages
-
-
-print('gc.collect()')
-gc.collect()
+esp.osdebug(None) # turn off vendor O/S debugging messages
+esp.sleep_type(esp.SLEEP_NONE) # Don't go into sleep mode
 
 
 print('boot.py END')
