57a8e52f950d794b34f593494b71efb228714a91
JensDiemer <git@jensdiemer.de>
2019-12-05 22:50:37 +0100

    split watchdog

--- src/watchdog.py
+++ src/watchdog.py
@@ -2,42 +2,11 @@ import gc
 
 import constants
 import machine
-import uerrno as errno
-import usocket as socket
 import utime as time
 from micropython import const
-from rtc import incr_rtc_count, rtc_isoformat, get_rtc_value
-
-WATCHDOG_TIMEOUT = const(30)
+from rtc import get_rtc_value, rtc_isoformat
 
 _CHECK_PERIOD = const(50 * 1000)  # 50 sec
-_MIN_FREE = const(2 * 1024)
-
-
-def reset(reason):
-    print('Watchdog reset reason: %s' % reason)
-    incr_rtc_count(key=constants.RTC_KEY_WATCHDOG_COUNT)
-    from reset import ResetDevice
-    ResetDevice(reason=reason).reset()
-
-
-def can_bind_web_server_port():
-    server_address = (constants.WEBSERVER_HOST, constants.WEBSERVER_PORT)
-    sock = socket.socket()
-    try:
-        sock.settimeout(1)
-        try:
-            sock.bind(server_address)
-        except OSError as e:
-            # If webserver is running:
-            # [Errno 98] EADDRINUSE
-            if e.args[0] == errno.EADDRINUSE:
-                return False
-        else:
-            print('ERROR: Web server not running! (Can bind to %s:%i)' % server_address)
-            return True
-    finally:
-        sock.close()
 
 
 class Watchdog:
@@ -59,23 +28,10 @@ class Watchdog:
         )
 
     def _timer_callback(self, timer):
+        from watchdog_checks import check
+        check(last_feed=self.last_feed, wifi=self.wifi)
+        del check
         gc.collect()
-
-        if gc.mem_free() < _MIN_FREE:
-            reset(reason='RAM full')
-
-        if not self.wifi.is_connected:
-            self.wifi.ensure_connection()
-
-        last_connection = time.time() - self.wifi.connected_time
-        if last_connection > constants.WIFI_TIMEOUT:
-            reset(reason='WiFi timeout')
-
-        if can_bind_web_server_port():
-            reset(reason='Web Server down')
-
-        if time.time() - self.last_feed > WATCHDOG_TIMEOUT:
-            reset(reason='Feed timeout')
 
         self.check_count += 1
         self.last_check = rtc_isoformat()
