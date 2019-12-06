682c9b7cd280281873abfc5883cc6b993b1b7731
JensDiemer <git@jensdiemer.de>
2019-12-06 21:16:56 +0100

    +flash_firmware.sh

---
+++ flash_firmware.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# http://docs.micropython.org/en/latest/esp8266/tutorial/intro.html#deploying-the-firmware
+#
+# Download firmware from:
+#
+# http://micropython.org/download#esp8266
+#
+# setup user rights:
+#
+# sudo usermod -G dialout -a $USER
+#
+# 1. press and hold power button
+# 2. then, connect to PC
+# 3. start this script
+#
+#
+# First "erase_flash" is needed, then "write_flash"
+# but both steps need the reconnect to PC !
+#
+
+set -ex
+
+FIRMWARE='esp8266-20190529-v1.11.bin'
+
+#pipenv run esptool.py --port /dev/ttyUSB0 erase_flash
+pipenv run esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 ${FIRMWARE}
