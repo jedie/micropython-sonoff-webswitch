a04f021f701c79f0a09f8c1d41f84945db0366d0
JensDiemer <git@jensdiemer.de>
2019-12-14 10:43:06 +0100

    move parse_request()

--- src/http_utils.py
+++ src/http_utils.py
@@ -16,3 +16,42 @@ async def send_error(writer, reason, http_code=404):
     await writer.awrite(b'HTTP/1.0 %i\r\n' % http_code)
     await writer.awrite(b'Content-type: text/plain; charset=utf-8\r\n\r\n')
     await writer.awrite(str(reason))
+
+
+async def parse_request(reader):
+    method, url, http_version = (await reader.readline()).decode().strip().split()
+    # print(http_version)
+
+    if '?' in url:
+        url, querystring = url.split('?', 1)
+    else:
+        querystring = None
+
+    # Consume all headers but use only content-length
+    content_length = None
+    while True:
+        line = await reader.readline()
+        if line == b'\r\n':
+            break  # header ends
+
+        try:
+            header, value = line.split(b':', 1)
+        except ValueError:
+            break
+
+        value = value.strip()
+
+        if header == b'Content-Length':
+            content_length = int(value.decode())
+
+        # print(header, value)
+
+    print('content length:', content_length)
+
+    # get body
+    if content_length:
+        body = (await reader.read(content_length)).decode()
+    else:
+        body = None
+
+    return method, url, querystring, body
--- src/webswitch.py
+++ src/webswitch.py
@@ -73,53 +73,18 @@ class WebServer:
         del sys.modules[module_name]
         gc.collect()
 
-    async def parse_request(self, reader):
-        method, url, http_version = (await reader.readline()).decode().strip().split()
-        # print(http_version)
-
-        if '?' in url:
-            url, querystring = url.split('?', 1)
-        else:
-            querystring = None
-
-        # Consume all headers but use only content-length
-        content_length = None
-        while True:
-            line = await reader.readline()
-            if line == b'\r\n':
-                break  # header ends
-
-            try:
-                header, value = line.split(b':', 1)
-            except ValueError:
-                break
-
-            value = value.strip()
-
-            if header == b'Content-Length':
-                content_length = int(value.decode())
-
-            # print(header, value)
-
-        print('content length:', content_length)
-
-        # get body
-        if content_length:
-            body = (await reader.read(content_length)).decode()
-        else:
-            body = None
-
-        return method, url, querystring, body
-
     async def send_response(self, reader, writer):
         print('\nRequest from:', writer.get_extra_info('peername'))
 
+        from http_utils import parse_request
         try:
-            method, url, querystring, body = await self.parse_request(reader)
+            method, url, querystring, body = await parse_request(reader)
         except ValueError as e:
             self.message = str(e)
             url = '/'  # redirect
 
+        del parse_request
+        del sys.modules['http_utils']
         gc.collect()
 
         if url == '/':
