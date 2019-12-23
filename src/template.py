07c47f8b15882dd6d91cf7df507c087623ff0143
JensDiemer <git@jensdiemer.de>
2019-12-23 15:19:45 +0100

    add gc.collect() to render()

--- src/template.py
+++ src/template.py
@@ -1,9 +1,13 @@
+import gc
+
+
 def render(filename, context, content_iterator=None):
+    gc.collect()
     with open(filename, 'r') as f:
         while True:
             line = f.readline()
             if not line:
-                return
+                break
 
             if content_iterator is not None and '{% content %}' in line:
                 for content_line in content_iterator:
@@ -13,3 +17,5 @@ def render(filename, context, content_iterator=None):
                     yield line.format(**context)
                 except Exception as e:
                     yield 'ERROR: %r' % e
+
+    gc.collect()
